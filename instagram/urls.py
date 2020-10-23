2c6ff7e638c8e83a3c06826a8327abce9f73064e
Akumucollins <akumucollins001@gmail.com>
2020-10-23 04:02:55 +0300

    created a urlpatterns for about, home, create post, post detail, post form, update post and delete post

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,18 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
-app_name = 'instagram'
 urlpatterns = [
-    # path('', views.home,name='home'),
+    path('', PostListView.as_view(), name='instagram-home'),
+    path('about/',views.about, name='instagram-about'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('<int:pk>',views.like, name='like'),
 ]
-if settings.DEBUG:
-    urlpatterns+= static(
-        settings.MEDIA_URL, 
-        document_root = settings.MEDIA_ROOT
-    )
-if settings.DEBUG:
-    urlpatterns+= static(
-        settings.STATIC_URL, 
-        document_root = settings.STATIC_ROOT)    
